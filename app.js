741272d84b6293b7caec63b4b620ae90c71751a2
kmurp62rulz <kmurp62rulz@gmail.com>
2022-07-31 09:31:17 +1000

    somewhat fixed window sizing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const inputs = require("./inputs.json");
+const iPhone = puppeteer.devices['iPhone X'];
 
 const convertWordToInputs = (word) => {
   let thingsToInput = [];
@@ -9,66 +10,74 @@ const convertWordToInputs = (word) => {
   return thingsToInput;
 };
 
-const asyncInputWord = function (page,word) {
+const asyncInputWord = (page, word) => {
   return new Promise(async (resolve, reject) => {
     for (let i = 0; i < convertWordToInputs(word).length; i++) {
-    await page.click(
-      `#wordle-app-game > div.Keyboard-module_keyboard__1HSnn > div:nth-child(${convertWordToInputs(word)[i][0]}) > button:nth-child(${convertWordToInputs(word)[i][1]})`
-    );
-    await page.waitForTimeout(500);
-  }
+      await page.click(
+        `#wordle-app-game > div.Keyboard-module_keyboard__1HSnn > div:nth-child(${
+          convertWordToInputs(word)[i][0]
+        }) > button:nth-child(${convertWordToInputs(word)[i][1]})`
+      );
+      await page.waitForTimeout(500);
+    }
 
-  await page.waitForTimeout(2000);
+    await page.waitForTimeout(2000);
 
-  let urls = await page.evaluate(() => {
-    let results = [];
-    let items = document.querySelectorAll("div.Tile-module_tile__3ayIZ");
-    items.forEach((item) => {
-      if (item.getAttribute("data-state") != 'empty') {
-        results.push({
-          text: item.innerText,
-          state: item.getAttribute("data-state"),
-        });
-      }
+    let urls = await page.evaluate(() => {
+      let results = [];
+      let items = document.querySelectorAll("div.Tile-module_tile__3ayIZ");
+      items.forEach((item) => {
+        if (item.getAttribute("data-state") != "empty") {
+          results.push({
+            text: item.innerText,
+            state: item.getAttribute("data-state"),
+          });
+        }
+      });
+
+      return results;
     });
+    return resolve(urls);
+  }).catch((err) => console.log(err));
+};
 
-    return results;
-  });
-  return resolve(urls);
-  }).catch(err => console.log(err))
-}
+const determineBestWord = (urls) => {
+  return "clown?";
+};
 
-
-
-
-function run() {
-
+const run = () => {
   return new Promise(async (resolve, reject) => {
     try {
-      const browser = await puppeteer.launch({ headless: false });
+      const browser = await puppeteer.launch({
+        headless: false,
+        args: [`--window-size=450,950`],
+        defaultViewport: {
+          width: 450,
+          height: 900,
+          isMobile: true,
+        }
+        
+      });
       const page = await browser.newPage();
+      await page.emulate(iPhone);
       await page.goto("https://www.nytimes.com/games/wordle/index.html");
       await page.click(
         "#wordle-app-game > div.Modal-module_modalOverlay__81ZCi.Modal-module_enableAuth__SR682 > div > div > svg"
       );
       await page.waitForTimeout(1000);
 
+      let first = await asyncInputWord(page, "roate?");
+      let second = await asyncInputWord(page, determineBestWord(first));
+      let third = await asyncInputWord(page, "dream?");
+      let fourth = await asyncInputWord(page, "rager?");
+      let fifth = await asyncInputWord(page, "lover?");
+      let final = await asyncInputWord(page, "final?");
 
-      // let first = await asyncInputWord(page, "roate?")
-      // let second = await asyncInputWord(page, "floor?")
-      // let third = await asyncInputWord(page, "dream?")
-      // let fourth = await asyncInputWord(page, "rager?")
-      let fifth = await asyncInputWord(page, "lover?")
-      let final = await asyncInputWord(page, "final?")
-
-
-      browser.close();
+      // browser.close();
       return resolve(final);
-      
-
     } catch (e) {
       return reject(e);
     }
   });
-}
+};
 run().then(console.log).catch(console.error);
